2b2168f145d62fcbecac89397eb4f15a143df501
kidus-feleke <kidusfeleke5@gmail.com>
2023-02-02 15:20:50 +0300

    Added redux persist to the redux store

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import store from './redux/store';
-
+import {store} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import {persistor} from './redux/store';
 
 
  const root = ReactDOM.createRoot(document.getElementById('root'));
  root.render( 
    <Provider store={store}>
      <BrowserRouter> 
-       <App />
+     <PersistGate loading={null} persistor={persistor}>
+     <App />
+     </PersistGate>
      </BrowserRouter>
    </Provider>
      
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,27 @@
 //import { applyMiddleware } from "redux";
-import { configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore} from "@reduxjs/toolkit";
+import {persistStore,persistReducer,} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import  userReducer  from './user/userSlice';
-import CartReducer from './Cart/CartSlice'
-//import logger from "redux-logger";
+import CartReducer from './Cart/CartSlice';
+import thunk from "redux-thunk";
 
+const persistConfig={
+    key:'root',
+    storage,
+}
+const rootReducer=combineReducers({
+    user:userReducer,
+    cart:CartReducer
+});
 
-const store= configureStore({
-    reducer:{
-       user:userReducer,
-       cart:CartReducer
-    },
-    
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({serializableCheck:false}).concat(logger)
-    
-    
+const persistedReducer=persistReducer(persistConfig,rootReducer)
+
+export const store= configureStore({
+    reducer:persistedReducer,
+    // middleware:[thunk]
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({serializableCheck:false}).concat(logger)  
 })
+export const persistor=persistStore(store)
 
-export default store;
